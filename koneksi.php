67cd4e5c0aa2e5327bd9a240f7d4c2db3c711da1
rifaldo <muhammadrifaldosaputra@gmail.com>
2026-01-17 11:20:27 +0700

    feat: buat koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$port = 3306;
+$user = "root";
+$password = "";
+$database = "belajarcrud";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
